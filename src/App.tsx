9e9de8bb7c20e221c613d6d8ddb4aeea95bd793c
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-08 23:40:32 -0400

    laying out html elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <main>
+      <section className="Selector">
+        <figure>
+          <i className="fas fa-lightbulb"></i>
+          <p>lightbulb</p>
+        </figure>
+
+        <p>Hue</p>
+        <input className="hue" type="range" min="0" max="360" value="180" />
+        <p>Saturation</p>
+        <input
+          className="saturation"
+          type="range"
+          min="0%"
+          max="100%"
+          value="50%"
+        />
+        <p>Light</p>
+        <input className="light" type="range" min="0%" max="100%" value="50%" />
+      </section>
+    </main>
+  )
 }
