b56ce2b43157219ebbe84d3bccfe721f2bfaf858
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-09 17:35:54 -0400

    added Font Awesome packages

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,19 @@ import React from 'react'
 export function App() {
   return (
     <main>
+      <section className="title">
+        <h1>Color Picker</h1>
+      </section>
+
       <section className="Selector">
         <figure>
           <i className="fas fa-lightbulb"></i>
-          <p>lightbulb</p>
         </figure>
 
         <p>Hue</p>
         <input className="hue" type="range" min="0" max="360" value="180" />
         <p>Saturation</p>
-        <input
-          className="saturation"
-          type="range"
-          min="0%"
-          max="100%"
-          value="50%"
-        />
+        <input className="saturation" type="range" min="0%" max="100%" value="50%" />
         <p>Light</p>
         <input className="light" type="range" min="0%" max="100%" value="50%" />
       </section>
